e4f08543b32afda5320d9ea321eaf158fde4e9c2
Nimotomato <108477753+nimotomato@users.noreply.github.com>
2023-03-30 17:07:57 +0200

    test

--- src/context/socket.js
+++ src/context/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 import { createContext } from "react";
 
-export const socket = io.connect("http://localhost:3001");
+export const socket = io.connect(SERVER_URL);
 export const SocketContext = createContext();
